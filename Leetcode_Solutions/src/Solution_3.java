24e591d4c4c5833384777e1972dee645594dafc5
basila1 <basila_n@outlook.com>
2017-11-21 18:52:16 -0800

    refactor solution 3

--- Leetcode_Solutions/src/Solution_3.java
+++ Leetcode_Solutions/src/Solution_3.java
@@ -4,6 +4,7 @@ import java.util.Map;
 public class Solution_3 {
 	public int lengthOfLongestSubstring(String s) {
 		if (s == null && s.length() == 0) return 0;
+		if(s.length() == 1) return 1;
 		int result = 0, begin = 0;
 		Map<Character, Integer> m = new HashMap<Character, Integer>();
 		for(int i = 0; i < s.length(); i++) {
