18e87c7b268cc7cd5408e36317ed4db3f6c97850
basila1 <basila_n@outlook.com>
2017-11-21 17:49:16 -0800

    Two Sum

---
+++ 1_TwoSum/src/Solution.java
@@ -0,0 +1,43 @@
+/**
+ * Time : O(N) ; Space: O(N)
+ * @tag : Array; Hash Table
+ * @by  : Basila Nathan
+ * @date: 11/21/17
+ ***************************************************************************
+ * Description:
+ * Given an array of integers, find two numbers such that they add up to 
+ * a specific target number. 
+ * The function twoSum should return indices of the two numbers such that 
+ * they add up to the target, where index1 must be less than index2. 
+ * Please note that your returned answers (both index1 and index2) are not 
+ * zero-based. 
+ * 
+ * You may assume that each input would have exactly one solution. 
+ * Input: numbers={2, 7, 11, 15}, target=9 
+ * Output: index1=1, index2=2
+ * 
+ ***************************************************************************
+ * {@link https://leetcode.com/problems/two-sum/ }
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                break;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return result;
+    }
+
+}
