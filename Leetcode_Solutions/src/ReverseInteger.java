34fa233244f0c783b755f7e8ca7b0f09cb026454
basila1 <basila_n@outlook.com>
2017-11-28 19:31:18 -0800

    merge linked lists

--- Leetcode_Solutions/src/ReverseInteger.java
+++ Leetcode_Solutions/src/ReverseInteger.java
@@ -1,3 +1,21 @@
+/**
+ * Time : O(n); Space : O(1)
+ * @tag : Math
+ * @by  : Basila Nathan
+ * @date: 11/22/2017
+ *************************************************************************
+ * Description: 
+ * 
+ * Reverse digits of an integer. 
+ * 
+ * Example1: x = 123, return 321
+ * Example2: x = -123, return -321 
+ * 
+ * Return 0 if the result overflows and does not fit in a 32 bit signed integer
+ *
+ *************************************************************************
+ * {@link https://leetcode.com/problems/reverse-integer/ }
+ */
 
 public class ReverseInteger {
 
