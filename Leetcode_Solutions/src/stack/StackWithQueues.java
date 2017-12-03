8bbe0fb51693ea0e1ebb2ee8bbaf0f8fa768c85b
basila1 <basila_n@outlook.com>
2017-12-02 18:53:12 -0800

    refactor using two queues

--- Leetcode_Solutions/src/stack/StackWithQueues.java
+++ Leetcode_Solutions/src/stack/StackWithQueues.java
@@ -8,6 +8,9 @@ import java.util.Queue;
  * @author basila
  * 
  * (One Queue, push - O(n), pop O(1) )
+ * (Two Queues, push - O(1), pop O(n) )
+ * 
+ * https://leetcode.com/articles/implement-stack-using-queues/
  *
  */
 
@@ -21,7 +24,9 @@ public class StackWithQueues {
 
 	push 5 ---> 3,5
 	->reverse: 5,3
-
+	
+            i   2 1 0
+                
 	push 2 ---> 5,3,2
 	->reverse: 2,5,3
 
@@ -50,4 +55,40 @@ public class StackWithQueues {
 		return q1.isEmpty();
 	}
 
+}
+
+//second solution using two queue
+
+pubilc class StackWithQueue {
+	private Queue<Integer> q1 = new LinkedList<Integer>();
+	private Queue<Integer> q2 = new LinkedList<Integer>();
+	private int top;
+	
+	// Push element x onto stack.
+	public void push(int x) {
+	    q1.add(x);
+	    top = x;
+	}
+	
+	// Removes the element on top of the stack.
+	public void pop() {
+	    while (q1.size() > 1) {
+	        top = q1.remove();
+	        q2.add(top);
+	    }
+	    q1.remove();
+	    Queue<Integer> temp = q1;
+	    q1 = q2;
+	    q2 = temp;
+	}
+	
+	public boolean empty() {
+	    return q1.isEmpty() && q2.isEmpty();
+	}
+	
+	public int top() {
+		return top;
+	}
+	
+	
 }
