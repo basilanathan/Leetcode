b94bb6d03f46654e534b8893ae18009d00f0683b
basila1 <basila_n@outlook.com>
2017-12-09 18:32:37 -0800

    101. Symmetric Tree Iterative

--- Leetcode_Solutions/src/bst/SymmetricTree.java
+++ Leetcode_Solutions/src/bst/SymmetricTree.java
@@ -1,5 +1,7 @@
 package bst;
 
+import java.util.Stack;
+
 /**
  * 
  * @author basila
@@ -32,5 +34,23 @@ public class SymmetricTree {
 			return false;
 	}
 	
+	//iterative solution
+	public boolean isSymmetricIterative(TreeNode root) {
+		if(root == null) return true;
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(root.left);
+		stack.push(root.right);
+		
+		while(!stack.isEmpty()) {
+			TreeNode n1 = stack.pop(), n2 = stack.pop();
+			if(n1 == null && n2 == null) continue;
+			if(n1 == null || n2 == null || n1.val != n2.val) return false;
+	        stack.push(n1.left);
+	        stack.push(n2.right);
+	        stack.push(n1.right);
+	        stack.push(n2.left);
+		}
+		return true;
+	}
 
 }
