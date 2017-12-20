3c29aa7e12288c47f83ab054e39be106e6d49177
basila1 <basila_n@outlook.com>
2017-12-19 17:38:05 -0800

    5. Longest Palindromic Substring

--- Leetcode_Solutions/src/bst/MinimumDepthBinaryTree.java
+++ Leetcode_Solutions/src/bst/MinimumDepthBinaryTree.java
@@ -18,5 +18,14 @@ public class MinimumDepthBinaryTree {
 		
 		return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
 	}
-
+	
+	//alternate solution
+	public int minDepthAlt(TreeNode root) {
+	    if (root == null)
+	        return 0;
+	    if (root.left != null && root.right != null)
+	        return Math.min(minDepth(root.left), minDepth(root.right))+1;
+	    else
+	        return Math.max(minDepth(root.left), minDepth(root.right))+1;
+	}
 }
---
+++ Leetcode_Solutions/src/strings/LPS.java
@@ -0,0 +1,37 @@
+package strings;
+
+public class LPS {
+	
+	//G E E K E G
+	private static int lo;
+	private static int maxLength;
+	
+	public static String longestPalindrome(String s) {
+		int len = s.length();
+		if (len < 2) return s;
+		
+		for(int i = 0; i < len - 1; i++) {
+			extendPalindrome(s, i, i); //odd number -> one character for middle
+			extendPalindrome(s, i, i + 1); //even number -> two characters for middle
+		}
+		return s.substring(lo, lo + maxLength);
+	}
+
+	private static void extendPalindrome(String s, int j, int k) {
+		
+		while(j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+			j--;
+			k++;
+		}
+		if (maxLength < k - j - 1) {
+			lo = j + 1;
+			maxLength = k - j - 1;
+		}
+	}
+	
+	public static void main(String[] args) {
+		String longest = longestPalindrome("geekeg");
+		System.out.println(longest);
+	}
+
+}
